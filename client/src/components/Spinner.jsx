7fdb38e6103af680622bedc6ad37fd2f81ca6159
abitshrestha <abitshrestha2080@gmail.com>
2024-02-25 15:00:11 +0545

    fsdf

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -4,7 +4,7 @@ const Spinner = () => {
   return (
     <div className='d-flex justifiy-content-center'>
         <div className='spinner-border' role='status'>
-            <span className='sr-only'></span>
+            <span className='sr-only text-center'></span>
         </div>
     </div>
   )
